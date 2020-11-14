28d1a69e8c1493238d45f0579105bd4e61565180
ferhat.guener <nyufait@msn.com>
2020-11-14 10:55:33 +0100

    Update

--- 4-1-simple_linear_regression/simple_linear_regression.py
+++ 4-1-simple_linear_regression/simple_linear_regression.py
@@ -5,35 +5,38 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-# TODO import dataset
+# import dataset
 dataset = pd.read_csv('salary_data.csv')
-X = ...
-y = ...
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
 
-# TODO split the dataset into training and test set (use train_test_split(...))
-X_train, X_test, y_train, y_test = ...
+# split the dataset into training and test set (use train_test_split(...))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 / 3)
 
-# TODO fit Simple Linear Regression to the training set
-regressor = ...
-regressor.fit(...)
+# fit Simple Linear Regression to the training set
+learning_algo = LinearRegression()
+learning_algo.fit(X_train, y_train)  # this is where the learning happens
 
-# TODO predict the test set results
-y_pred = regressor....
+# predict the test set results
+y_pred = learning_algo.predict(X_test)
 
 # plot the training set results
 plt.scatter(X_train, y_train, color='red')
-plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.plot(X_train, learning_algo.predict(X_train), color='blue')
 plt.title('Salary vs Experience (Training set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
 
-# TODO plot the test set results (similar to training set resolts)
-plt.scatter(...)
+# plot the test set results (similar to training set resolts)
+plt.scatter(X_test, y_test)
 # plot the trained regression again to compare with the test set
-plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.plot(X_train, learning_algo.predict(X_train), color='blue')
 plt.title('Salary vs Experience (Test set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
 
+# show the prediction for input "x = 20"
+y_pred = learning_algo.predict([[20]])
+print(y_pred)
--- 4-2-polynomial_regression/polynomial_regression.py
+++ 4-2-polynomial_regression/polynomial_regression.py
@@ -2,11 +2,10 @@ import numpy as np
 from sklearn import linear_model
 from sklearn.preprocessing import PolynomialFeatures
 import plotly.express as px
-from sklearn.model_selection import train_test_split
-
 
 # this makes the plot show in the browser even if run from inside PyCharm (without this, the plot does not open)
 import plotly.io as pio
+
 pio.renderers.default = 'browser'
 
 # Load the Amazon dataset
@@ -19,41 +18,39 @@ amazon_y = dataset[:, np.newaxis, 1]
 # generate plot with input data
 fig = px.bar(x=amazon_X.flatten(), y=amazon_y.flatten())
 
-# TODO split the data into training/testing sets
-amazon_X_train, amazon_X_test, amazon_y_train, amazon_y_test = train_test_split(amazon_X, amazon_y, 1/3)
-
-# TODO add some more future x values to amazon_X_test to get predictions for (e.g. for 2021, 2022,...)
-# amazon_X_test = ...
+# add some more future x values to get predictions for
+amazon_X_pred = np.append(amazon_X, [[2021], [2022], [2023], [2024], [2025]], 0)
 
 # LINEAR REGRESSION
-# TODO create linear regression object
+# create linear regression object
 regression = linear_model.LinearRegression()
 
-# TODO train the model using the training sets
-regression.fit(amazon_X_train, amazon_y_train)
+# train the model using the training sets
+regression.fit(amazon_X, amazon_y)
 
-# TODO make predictions using the testing set
-amazon_y_pred = regression.predict(amazon_X_test)
+# make predictions using the new data points
+amazon_y_pred = regression.predict(amazon_X_pred)
 
-# TODO plot outputs
-fig.add_scatter(x=....flatten(), y=....flatten(), name='predictions (linear regression)')
+# plot outputs
+fig.add_scatter(x=amazon_X_pred.flatten(), y=amazon_y_pred.flatten(), name='predictions (linear regression)')
 
 # QUADRATIC REGRESSION
 # transform input data into format for quadratic polynomial
-# TODO generate polynomial transform
-poly = PolynomialFeatures(degree=...)
+poly = PolynomialFeatures(degree=2)
 # now transform all data
-X = poly.fit_transform(amazon_X_train)
-y = poly.fit_transform(amazon_y_train)
-X_test = poly.fit_transform(amazon_X_test)
+X = poly.fit_transform(amazon_X)
+y = poly.fit_transform(amazon_y)
+X_pred = poly.fit_transform(amazon_X_pred)
 
 # with this transformed data, the normal linear regression model can be used
-# TODO fit model
-regression.fit(...)
-# TODO make predictions using the testing set
-amazon_y_pred_poly = ...
+# fit model
+regression.fit(X, y)
+# make predictions using the new data points
+amazon_y_pred_poly = regression.predict(X_pred)
 
-# add another plot and show the whole figure
-fig.add_scatter(x=amazon_X_test.flatten(), y=amazon_y_pred_poly[:, 1].flatten(),
+# add another plot
+fig.add_scatter(x=amazon_X_pred.flatten(), y=amazon_y_pred_poly[:, 1].flatten(),
                 name='predictions (quadratic regression)')
+
+# show the whole plot
 fig.show()
